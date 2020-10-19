d94d99ae5e13be9d83918fc3d3ffbc52ab2e3c88
b.lajtar <bartolineon@o2.pl>
2020-10-19 03:41:36 +0200

    Updates

---
+++ precache-manifest.e40c970973b0beae08aa4bdae5f616c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf00e664d039bf65ab9e75d792efbae6",
+    "url": "/SongProgressApp/index.html"
+  },
+  {
+    "revision": "0748c6640946e7811644",
+    "url": "/SongProgressApp/static/css/main.4c53d40d.chunk.css"
+  },
+  {
+    "revision": "afd23d6d9769c112cbc1",
+    "url": "/SongProgressApp/static/js/2.110bce6a.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/SongProgressApp/static/js/2.110bce6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0748c6640946e7811644",
+    "url": "/SongProgressApp/static/js/main.4db573ca.chunk.js"
+  },
+  {
+    "revision": "b02df12580f179fbee24",
+    "url": "/SongProgressApp/static/js/runtime-main.b4461d9b.js"
+  }
+]);
